6b2f44b98fcc2223054411d695fbb9c1a2ef8a16
Bruce Molina <bruce.molina.81@gmail.com>
2017-08-17 02:58:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 const app = "I don't do much."
+
+// left is 37
+// right is 39
+
+document.addEventListener('keydown', function(e) {
+
+  if (e.which === 37){
+    var position = dodger.style.left.replace("px", "")
+
+    if (position > 0){
+      var left_one_pix = parseInt(position - 1 )
+    } else {
+      alert('cant go further!')
+      return 
+    }
+
+    dodger.style.left = `${left_one_pix.toString()}px`
+} else if (e.which === 39){
+
+  var position = dodger.style.left.replace("px", "")
+
+  if (position < 360){
+    var right_one_pix = parseInt(position) + 1
+  } else {
+    alert('cant go further!')
+    return
+  }
+
+  dodger.style.left = `${right_one_pix.toString()}px`
+  }
+})
